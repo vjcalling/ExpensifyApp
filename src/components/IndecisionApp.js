338c2fe9ed1cf6c4f261fca5f37488c0756404c2
vjcalling <vibhor.jain@outlook.com>
2018-04-30 15:06:55 +0530

    adding separate scss for all components

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -90,12 +90,14 @@ class IndecisionApp extends React.Component {
                 <Header title={title} subtitle={subtitle}/>
                 <div className="container">
                     <Action hasOptions={this.state.options.length > 0} handlePick={this.handlePick}/>
-                    <Options 
-                        options={this.state.options}
-                        handleDeleteOptions={this.handleDeleteOptions}
-                        handleDeleteOption={this.handleDeleteOption}
-                    />
-                    <AddOption handleAddOption={this.handleAddOption}/>
+                    <div className="widget">
+                        <Options 
+                            options={this.state.options}
+                            handleDeleteOptions={this.handleDeleteOptions}
+                            handleDeleteOption={this.handleDeleteOption}
+                        />
+                        <AddOption handleAddOption={this.handleAddOption}/>
+                    </div>
                 </div>
                 <OptionModal 
                     selectedOption={this.state.selectedOption}
--- src/components/Option.js
+++ src/components/Option.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Option = (props) => {
     return(
-        <div>
-            {props.option}
+        <div className="option">
+            <p className="option__text">{props.count}. {props.option}</p>
             <button 
+                className="button button--link"
                 onClick={(e) => {
                     props.handleDeleteOption(props.option);
                 }}

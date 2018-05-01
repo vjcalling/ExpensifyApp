04f5f93bc4c7150d1699a86634323b9fafaf9ab3
vjcalling <vibhor.jain@outlook.com>
2018-05-01 13:06:15 +0530

    adding Link component for client side routing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 //----------------------------------------
@@ -17,19 +17,47 @@ const AddExpensePage = () => (
     </div>
 );
 
+const EditExpensePage = () => (
+    <div>
+        This is from dashboard for edit
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is from dashboard for help
+    </div>
+);
+
+
 const NotFoundPage = () => (
     <div>
-        404!
+        404! - <Link to="/">Go Home!</Link>
     </div>
+);
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <Link to="/">Home</Link>
+        <Link to="/create">Create</Link>
+        <Link to="/edit">Edit</Link>
+        <Link to="/help">Help</Link>
+    </header>
 );
 
 const routes = (
     <BrowserRouter>
-        <Switch>
-            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-            <Route path="/create" component={AddExpensePage}/>
-            <Route component={NotFoundPage} />
-        </Switch>
+        <div>
+            <Header />
+            <Switch>
+                <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+                <Route path="/create" component={AddExpensePage}/>
+                <Route path="/edit" component={EditExpensePage}/>
+                <Route path="/help" component={HelpPage}/>
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
     </BrowserRouter>
     
 );

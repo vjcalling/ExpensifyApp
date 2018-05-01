98484fcb29b29179bd25344c3595c7f23d8d6789
vjcalling <vibhor.jain@outlook.com>
2018-05-01 12:29:36 +0530

    adding routes and returning index.html to handle client side routing

--- src/app.js
+++ src/app.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 //----------------------------------------
 
-ReactDOM.render(<p>This is expensify app </p>, document.getElementById("app"));
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from dashboard
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        This is from dashboard for create
+    </div>
+);
+
+const NotFoundPage = () => (
+    <div>
+        This is from dashboard for create
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+            <Route path="/create" component={AddExpensePage}/>
+        </div>
+    </BrowserRouter>
+    
+);
+
+ReactDOM.render(routes, document.getElementById("app"));

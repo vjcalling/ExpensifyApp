fc60f12e8b8b23d969713712a7b35eaa4d247a13
vjcalling <vibhor.jain@outlook.com>
2018-05-01 10:47:21 +0530

    First commit

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp'
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 //----------------------------------------
 
-ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+ReactDOM.render(<p>This is expensify app </p>, document.getElementById("app"));

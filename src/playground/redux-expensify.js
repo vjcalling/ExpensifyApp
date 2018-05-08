2efc208e099be9a352a201becb926a71468b1d8b
vjcalling <vibhor.jain@outlook.com>
2018-05-08 08:08:54 +0530

    adding support for spread operator for arrays

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,32 @@
 
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
+
+
+//--------------------------------------------------------------------------------------
+
+const addExpense = ({ description = '', note = '', amount = 0, createdAt = 0} = {}) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
+
+
+const removeExpense = ({id} = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+});
+
+//--------------------------------------------------------------------------------------
+
+
+//--------------------------------------------------------------------------------------
+
 
 //--------------------------------------------------------------------------------------
 
@@ -8,7 +35,10 @@ const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
 
     switch(action.type) {
-
+        case 'ADD_EXPENSE':
+            return [...state, action.expense];  //[state.concat(action.expense)];   --> changing to spread operator for better control.
+        case 'REMOVE_EXPENSE':
+            return state.filter(({id}) => id !== action.id);
         default:
             return state;
     }
@@ -43,7 +73,13 @@ const store = createStore(
     })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(addExpense({description: 'rent', amount:100}));
+const expenseTwo = store.dispatch(addExpense({description: 'coffee', amount:200}));
+store.dispatch(removeExpense({id: expenseOne.expense.id}));
 
 //--------------------------------------------------------------------------------------
 

963d9119c9c23285b46924fae36d3bbf11c3c65a
vjcalling <vibhor.jain@outlook.com>
2018-05-14 20:30:01 +0530

    adding expenses

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -167,6 +167,7 @@ store.subscribe(() => {
 
 const expenseOne = store.dispatch(addExpense({description: 'rent', amount:100, createdAt: -21000}));
 const expenseTwo = store.dispatch(addExpense({description: 'coffee', amount:200, createdAt: -1000}));
+const expenseTwo = store.dispatch(addExpense({description: 'candy', amount:150, createdAt: 5000}));
 
 // store.dispatch(removeExpense({id: expenseOne.expense.id}));
 // store.dispatch(editExpense(expenseTwo.expense.id, {amount: 500}));

33b26f2f3a7ea3ef1641cc9dee648666e005aec9
vjcalling <vibhor.jain@outlook.com>
2018-05-18 11:41:21 +0530

    adding expense list

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 
-import {addExpense} from './actions/expenses';
-import {editExpense} from './actions/expenses';
-import {setTextFilter} from './actions/filters';
-import {sortByAmount} from './actions/filters';
+import { addExpense } from './actions/expenses';
+import { editExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import { sortByAmount } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 
 
@@ -18,10 +19,16 @@ const store = configureStore();
 
 const expenseOne = store.dispatch(addExpense({description: 'water bill', amount:100, createdAt: -21000}));
 const expenseTwo = store.dispatch(addExpense({description: 'gas bill', amount:100, createdAt: -21000}));
-store.dispatch(setTextFilter('gas'));
+store.dispatch(setTextFilter('bill'));
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
 console.log(visibleExpenses);
 
-ReactDOM.render(<AppRouter/>, document.getElementById("app"));
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById("app"));
